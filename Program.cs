d8680810bc33b44e6f1a230e423f6374692dba7e
herringvoices <nalherring@gmail.com>
2025-01-15 10:46:41 -0600

    Add endpoint for getting all appointments--optionally filtering by stylistId

--- Program.cs
+++ Program.cs
@@ -27,4 +27,69 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//Appointments Endpoints
+
+// GET all appointments, optionally filtered by stylistId
+app.MapGet(
+    "/api/appointments",
+    async (HillarysHairCareDbContext db, int? stylistId) =>
+    {
+        // Query appointments, optionally filtering by stylistId
+        var query = db
+            .Appointments.Include(a => a.Customer)
+            .Include(a => a.Stylist)
+            .Include(a => a.AppointmentServices)
+            .ThenInclude(aps => aps.Service)
+            .AsQueryable();
+
+        if (stylistId.HasValue)
+        {
+            query = query.Where(a => a.StylistId == stylistId.Value);
+        }
+
+        var appointments = await query
+            .Select(a => new AppointmentDTO
+            {
+                Id = a.Id,
+                Title = a.Title,
+                Start = a.Start,
+                End = a.End,
+                AllDay = a.AllDay,
+                ExtendedProps = new AppointmentDTO.ExtendedPropsDTO
+                {
+                    CustomerId = a.CustomerId,
+                    Customer = new CustomerDTO
+                    {
+                        Id = a.Customer.Id,
+                        FirstName = a.Customer.FirstName,
+                        LastName = a.Customer.LastName,
+                        PhoneNumber = a.Customer.PhoneNumber,
+                        Email = a.Customer.Email,
+                    },
+                    StylistId = a.StylistId,
+                    Stylist = new StylistDTO
+                    {
+                        Id = a.Stylist.Id,
+                        FirstName = a.Stylist.FirstName,
+                        LastName = a.Stylist.LastName,
+                        Email = a.Stylist.Email,
+                        PhoneNumber = a.Stylist.PhoneNumber,
+                        IsActive = a.Stylist.IsActive,
+                    },
+                    Services = a
+                        .AppointmentServices.Select(aps => new ServiceDTO
+                        {
+                            Id = aps.Service.Id,
+                            Name = aps.Service.Name,
+                            Price = aps.Service.Price,
+                        })
+                        .ToList(),
+                },
+            })
+            .ToListAsync();
+
+        return Results.Ok(appointments);
+    }
+);
+
 app.Run();
